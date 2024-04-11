fe719da50a7a00aea18ca90d6a1a50868fc6ab34
ryanvanle <ryanle4321@gmail.com>
2024-04-11 06:14:33 -0700

    finished mode 3 and overall project

--- code/nightLight/nightlight/nightlight.ino
+++ code/nightLight/nightlight/nightlight.ino
@@ -1,5 +1,12 @@
 // Author: Ryan Le
-// Used code from CITE SOURCES HERE
+// No AI code used
+
+// Used code from Physical Computing Textbook
+// Sections used:
+// Debouncing switches: https://makeabilitylab.github.io/physcomp/arduino/#lesson-3-debouncing-switches 
+// RGB LED and hue library: https://makeabilitylab.github.io/physcomp/arduino/rgb-led.html
+// Piano lesson: https://makeabilitylab.github.io/physcomp/arduino/piano.html
+// Crossfading RGB LEDs: https://makeabilitylab.github.io/physcomp/arduino/rgb-led-fade.html
 
 #include "src/RGBConverter/RGBConverter.h"
 
@@ -8,99 +15,83 @@ const boolean COMMON_ANODE = false;
 const boolean PHOTOCELL_IS_R2_IN_VOLTAGE_DIVIDER = true; // set false if photocell is R1
 
 // pin settings
-const int RGB_RED_PIN = 6;
-const int RGB_GREEN_PIN  = 5;
-const int RGB_BLUE_PIN  = 3;
+const int RGB_RED_PIN = 3;
+const int RGB_GREEN_PIN  = 6;
+const int RGB_BLUE_PIN  = 5;
 const int PHOTOCELL_INPUT_PIN = A0;
-const int BUTTON_INPUT_PIN = 8;
+const int MODE_BUTTON_INPUT_PIN = 7;
+const int SECONDARY_BUTTON_INPUT_PIN = 8;
 const int ROTATION_INPUT_PIN = A1;
-
-// button mode settings
-const int DEBOUNCE_WINDOW = 100;
-int _prevRawButtonVal = LOW;
-int _debouncedButtonVal = LOW;
-unsigned long _buttonStateChangeTimestamp = 0;  // the last time the input pin was toggled
+const int BUZZER_OUTPUT_PIN = 12;
 
 // photocell sensor settings
-const int MIN_PHOTOCELL_VAL = 550; // turn on LED
-const int MAX_PHOTOCELL_VAL = 900; // fully on value
+const int MIN_PHOTOCELL_VAL = 350; // turn on LED
+const int MAX_PHOTOCELL_VAL = 1023; // fully on value
 
 // LED settings
-const int DELAY_INTERVAL = 50; // interval in ms between incrementing hues
+const int DELAY_INTERVAL = 5; // interval in ms between incrementing hues
 const byte MAX_RGB_VALUE = 255;
 float _hue = 0; //hue varies between 0 - 1
 float _step = 0.001f;
 RGBConverter _rgbConverter;
 
-int photocellValue = 0;
+// button mode settings
+boolean hasModePressedRecently = false;
+const int DEBOUNCE_WINDOW = 40;
+int _modePrevRawButtonVal = LOW;
+int _modeDebouncedButtonVal = LOW;
+unsigned long _modeButtonStateChangeTimestamp = 0;  // the last time the input pin was toggled
 
-// rotation
+// rotation lock button settings
+boolean hasLockPressedRecently = false;
+int _lockPrevRawButtonVal = LOW;
+int _lockDebouncedButtonVal = LOW;
+unsigned long _lockButtonStateChangeTimestamp = 0;  // the last time the input pin was toggled
+boolean isLocked = false;
 
+// rotation range
+const int MAX_VOUT = 1000;
+const int MIN_VOUT = 100;
 
 // general settings
 const float MAX_MODES = 3;
 int currentMode = 1;
+
+
+// game settings
+boolean isInGame = false;
+
 
 void setup() {
   pinMode(RGB_RED_PIN, OUTPUT);
   pinMode(RGB_GREEN_PIN, OUTPUT);
   pinMode(RGB_BLUE_PIN, OUTPUT);
   pinMode(PHOTOCELL_INPUT_PIN, INPUT);
-  pinMode(BUTTON_INPUT_PIN, INPUT);
+  pinMode(MODE_BUTTON_INPUT_PIN, INPUT);
+  pinMode(SECONDARY_BUTTON_INPUT_PIN, INPUT);
+  pinMode(BUZZER_OUTPUT_PIN, OUTPUT);
 
   Serial.begin(9600);
 }
 
 void loop() {
-
   updateMode();
+  Serial.print("mode ");
+  Serial.println(currentMode);
 
   if (currentMode == 1) {
     updatePhotocell();
   } else if (currentMode == 2) {
     updateRotation();
   } else if (currentMode == 3) {
+    updateGame();
     // tba; 
   }
 
   // Serial.print(currentMode);
   // Serial.print(",");
-  // Serial.println(_debouncedButtonVal);
+  // Serial.println(_leftDebouncedButtonVal);
 }
-
-
-
-const int MAX_VOUT = 1000;
-const int MIN_VOUT = 100;
-void updateRotation() {
-
-
-  int vOut = analogRead(ROTATION_INPUT_PIN);
-  photocellValue = analogRead(PHOTOCELL_INPUT_PIN);
-
-  double rotationHue = map(vOut, MIN_VOUT, MAX_VOUT, 0, 100) * 0.01;
-  float saturation = 1;
-  // float lightness = map(x/zphotocellValue, MIN_PHOTOCELL_VAL, MAX_PHOTOCELL_VAL, 0, 5) * 0.001;
-  float lightness = 0.1;
-
-  
-  // if (rotationHue > 1.0) {
-  //   rotationHue -= 1;
-  // }
-
-  Serial.print(vOut);
-  Serial.print(",");
-  Serial.println(rotationHue);
-
-  byte rgb[3];
-  _rgbConverter.hslToRgb(rotationHue, saturation, lightness, rgb);
-  setColor(rgb[0], rgb[1], rgb[2]); 
-  delay(DELAY_INTERVAL);
-  // delay();
-
-
-} 
-
 
 void updatePhotocell() {
   // Read the photo-sensitive resistor value. If you have the photocell resistor hooked 
@@ -108,7 +99,7 @@ void updatePhotocell() {
   // then higher values correspond to brightness. If you have the photocell hooked up as Rbottom 
   // in the voltage divider (that is, one leg of the photocell is connected to GND), then
   // higher values correspond to darkness.
-  photocellValue = analogRead(PHOTOCELL_INPUT_PIN);
+  float photocellValue = analogRead(PHOTOCELL_INPUT_PIN);
 
   // Convert current hue, saturation, and lightness to RGB
   // The library assumes hue, saturation, and lightness range from 0 - 1
@@ -117,7 +108,11 @@ void updatePhotocell() {
   byte rgb[3];
 
   // Remap the value for output. 
-  float lightness = map(photocellValue, MIN_PHOTOCELL_VAL, MAX_PHOTOCELL_VAL, 0, 5) * 0.1;
+  float lightness = map(photocellValue, MIN_PHOTOCELL_VAL, MAX_PHOTOCELL_VAL, 0, 500) * 0.001;
+  if (lightness < 0) lightness = 0;
+  if (lightness > 500) lightness = 1023;
+
+
   float saturation = 1;
   _rgbConverter.hslToRgb(_hue, saturation, lightness, rgb);
 
@@ -131,10 +126,14 @@ void updatePhotocell() {
     _hue = 0;
   }
 
+
   delay(DELAY_INTERVAL);
 
   // Print the raw photocell value and the converted led value (e,g., for Serial 
-  // Console and Serial Plotter)
+  // Console and Serial Plotter)]
+  // Serial.print(currentMode);
+  // Serial.print(",");
+
   Serial.print(photocellValue);
   Serial.print(",");
   Serial.println(lightness);
@@ -163,30 +162,33 @@ void setColor(int red, int green, int blue)
 }
 
 
+// NOTE: I could re-implement this code as a class but unfortunately I do not have the time with the confidence to do it correctly.
 
-boolean hasPressedRecently = false;
 
 void updateMode() {
   // Read the button value. We assume a pull-down resistor button configuration so
   // the button will be HIGH when pressed and LOW when not pressed
-  int rawButtonVal = digitalRead(BUTTON_INPUT_PIN);
+  int rawButtonVal = digitalRead(MODE_BUTTON_INPUT_PIN);
 
   // If the button state has changed since the last reading, grab timestamp
   // This state change may be due to a legitimate user action or noise
-  if(rawButtonVal != _prevRawButtonVal){
-    _buttonStateChangeTimestamp = millis();
+  if(rawButtonVal != _modePrevRawButtonVal){
+    _modeButtonStateChangeTimestamp = millis();
   }
 
-  if((millis() - _buttonStateChangeTimestamp) >= DEBOUNCE_WINDOW){
+  if((millis() - _modeButtonStateChangeTimestamp) >= DEBOUNCE_WINDOW){
     // If we're here, then the button state hasn't changed for at least DEBOUNCE_WINDOW
     // So, we must be in a steady state
-    _debouncedButtonVal = rawButtonVal;
+    _modeDebouncedButtonVal = rawButtonVal;
 
-    if (!hasPressedRecently && _debouncedButtonVal) {
-      hasPressedRecently = true;
+    if (!hasModePressedRecently && _modeDebouncedButtonVal  && (millis() - _modeButtonStateChangeTimestamp) >= 1000) {
+      hasModePressedRecently = true;
+      isLocked = false;
+      noTone(BUZZER_OUTPUT_PIN);
+      isInGame = false;
       currentMode++;
-    } else if (!_debouncedButtonVal) {
-      hasPressedRecently = false;
+    } else if (!_modeDebouncedButtonVal) {
+      hasModePressedRecently = false;
     }
 
     if (currentMode > MAX_MODES) {
@@ -195,6 +197,145 @@ void updateMode() {
 
   }
 
-  _prevRawButtonVal = rawButtonVal;
+  _modePrevRawButtonVal = rawButtonVal;
 }
 
+
+void updateLock() {
+  int rawButtonVal = digitalRead(SECONDARY_BUTTON_INPUT_PIN);
+
+  if(rawButtonVal != _lockPrevRawButtonVal){
+    _lockButtonStateChangeTimestamp = millis();
+  }
+
+  if((millis() - _lockButtonStateChangeTimestamp) >= DEBOUNCE_WINDOW){
+    _lockDebouncedButtonVal = rawButtonVal;
+
+    if (!hasLockPressedRecently && _lockDebouncedButtonVal) {
+      hasLockPressedRecently = true;
+      isLocked = !isLocked;
+    } else if (!_lockDebouncedButtonVal) {
+      hasLockPressedRecently = false;
+    }
+  }
+
+  // Serial.println(isLocked);
+
+  _lockPrevRawButtonVal = rawButtonVal;
+}
+
+void updateRotation() {
+
+  updateLock();
+  // Serial.println(isLocked);
+  if (isLocked) return;
+  
+  int vOut = analogRead(ROTATION_INPUT_PIN);
+
+  double rotationHue = map(vOut, MIN_VOUT, MAX_VOUT, 0, 100) * 0.01;
+  float saturation = 1;
+  float lightness = 0.1;
+
+  if (rotationHue < 0) rotationHue = 0;
+
+  Serial.print(vOut);
+  Serial.print(",");
+  Serial.println(rotationHue);
+
+  byte rgb[3];
+  _rgbConverter.hslToRgb(rotationHue, saturation, lightness, rgb);
+  setColor(rgb[0], rgb[1], rgb[2]); 
+  delay(DELAY_INTERVAL);
+} 
+
+
+int currentColorIndex = 0;
+
+void updateGame() {
+
+  // red high low low
+  // green low high low
+  // blue low low high;
+
+  int colors[][3] = {{255, 0, 0}, {0, 255, 0}, {0, 0, 255}};
+
+  if (!isInGame) {
+    tone(BUZZER_OUTPUT_PIN, 262, 20);
+    setColor(colors[0][0], colors[0][1], colors[0][2]); 
+    delay(1000);
+
+    tone(BUZZER_OUTPUT_PIN, 262, 20);
+    setColor(colors[2][0], colors[2][1], colors[2][2]); 
+    delay(1000);
+
+    tone(BUZZER_OUTPUT_PIN, 1210, 500);
+    setColor(colors[1][0], colors[1][1], colors[1][2]); 
+
+    delay(200);
+    setColor(0, 0, 0); 
+    delay(1000);
+
+    currentColorIndex = random(3);
+    setColor(colors[currentColorIndex][0], colors[currentColorIndex][1], colors[currentColorIndex][2]); 
+
+    isInGame = true;
+
+  }
+
+  // left red
+  // right green
+  // both blue
+
+  boolean leftButtonValue = isLeftPressed();
+  boolean rightButtonValue = isRightPressed();
+  boolean isRed = currentColorIndex == 0;
+  boolean isGreen = currentColorIndex == 1;
+  boolean isBlue = currentColorIndex == 2;
+
+  boolean isCorrectAnswer = (leftButtonValue && rightButtonValue && isBlue) || (leftButtonValue && isRed) || (rightButtonValue && isGreen);
+
+  if (isCorrectAnswer) {
+    tone(BUZZER_OUTPUT_PIN, 1210, 500);
+    currentColorIndex = random(3);
+    setColor(colors[currentColorIndex][0], colors[currentColorIndex][1], colors[currentColorIndex][2]); 
+  }
+  
+}
+
+int _leftPrevRawButtonVal = LOW;
+int _leftDebouncedButtonVal = LOW;
+unsigned long _leftButtonStateChangeTimestamp = 0;  // the last time the input pin was toggled
+boolean isLeftPressed() {
+
+  int rawButtonVal = digitalRead(SECONDARY_BUTTON_INPUT_PIN);
+
+  if(rawButtonVal != _leftPrevRawButtonVal){
+    _leftButtonStateChangeTimestamp = millis();
+  }
+
+  if((millis() - _leftButtonStateChangeTimestamp) >= DEBOUNCE_WINDOW){
+    _leftDebouncedButtonVal = rawButtonVal;
+  }
+
+  _leftPrevRawButtonVal = rawButtonVal;
+  return _leftDebouncedButtonVal == HIGH;
+}
+
+int _rightPrevRawButtonVal = LOW;
+int _rightDebouncedButtonVal = LOW;
+unsigned long _rightButtonStateChangeTimestamp = 0;  // the last time the input pin was toggled
+boolean isRightPressed() {
+
+  int rawButtonVal = digitalRead(MODE_BUTTON_INPUT_PIN);
+
+  if(rawButtonVal != _rightPrevRawButtonVal){
+    _rightButtonStateChangeTimestamp = millis();
+  }
+
+  if((millis() - _rightButtonStateChangeTimestamp) >= DEBOUNCE_WINDOW){
+    _rightDebouncedButtonVal = rawButtonVal;
+  }
+
+  _rightPrevRawButtonVal = rawButtonVal;
+  return _rightDebouncedButtonVal == HIGH;
+}
